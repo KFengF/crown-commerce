34fa7e330a7f2df5eb90e5d24e8e5f40353151a8
Kevin Feng <fengfengkevin@gmail.com>
2019-11-16 18:45:59 -0400

    configured to PWA and enforced to https

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -15,6 +16,8 @@ app.use(compression());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.use(cors());
 
@@ -29,6 +32,10 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
     if (error) throw error;
     console.log('Server running on port ' + port);
+});
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
